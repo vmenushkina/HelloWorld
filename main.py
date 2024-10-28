bdf52be2301d922e99283063d967d5fe8db2f3ba
vmenushkina <kurs.puthon@gmail.com>
2024-10-28 23:37:46 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+# print ('"Hello, world!"')     # вывод в двойных кавычках " "
+# print ("'Hello, world!'")     # вывод в одинарных кавычках ' '
+# print ("Hello, world!")       # вывод без кавычек с использованием двойных
+# print ('Hello, world!')       # вывод без кавычек с использованием одинарных
+# print ('Hello, "world!"')     # вывод в двойных кавычках "world!"
+# print ("Hello, 'world!'")     # вывод в одинарных кавычках 'world!'
+
+print ("Hello world")           # по заданию: в файле main.py Напишите команду print("Hello world")
